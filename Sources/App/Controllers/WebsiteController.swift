ad22970d09bf2adf03a8bee61895a3c1f6bf3236
ChrisSullivan <cr_sullivan@hotmail.com>
2018-06-28 20:32:43 +1200

    Beautify the pages and add navigation

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -33,6 +33,8 @@ struct WebsiteController: RouteCollection {
   func boot(router: Router) throws {
     router.get(use: indexHandler)
     router.get("acronyms", Acronym.parameter, use: acronymHandler)
+    router.get("users", User.parameter, use: userHandler)
+    router.get("users", use: allUsersHandler)
   }
   
   func indexHandler(_ req: Request) throws -> Future<View> {
@@ -59,6 +61,37 @@ struct WebsiteController: RouteCollection {
         }
     }
   }
+    
+    func userHandler(_ req: Request) throws -> Future<View> {
+        // 2
+        return try req.parameters.next(User.self)
+            // 3
+            .flatMap(to: View.self) { user in
+                return try user.acronyms
+                    .query(on: req)
+                    // 4
+                    .all()
+                    .flatMap(to: View.self) { acronyms in
+                        let context = UserContext(title: user.name,
+                                                  user: user,
+                                                  acronyms: acronyms)
+                        return try req.view().render("user", context)
+                }
+         }
+    }
+    
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        // 2
+        return User.query(on: req)
+            .all()
+            // 3
+            .flatMap(to: View.self) { users in
+                let context = AllUsersContext(title: "All Users",
+                                              users: users)
+                return try req.view().render("allUsers", context)
+        }
+    }
+    
 }
 
 struct IndexContext: Encodable {
@@ -71,3 +104,16 @@ struct AcronymContext: Encodable {
   let acronym: Acronym
   let user: User
 }
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
+}
+
+
